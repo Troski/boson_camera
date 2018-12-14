299e58a003665d25e5780196be7e9cae22835be5
marco <marcoandarcia89@gmail.com>
2018-12-14 13:21:13 +0900

    changing pixel format

--- src/boson_camera.cpp
+++ src/boson_camera.cpp
@@ -71,8 +71,8 @@ void BosonCamera::init() {
 
     // Set video format
     format.type = V4L2_BUF_TYPE_VIDEO_CAPTURE;
-//    format.fmt.pix.pixelformat = V4L2_PIX_FMT_YVU420;    // 8-bit
-    format.fmt.pix.pixelformat = V4L2_PIX_FMT_Y16;       // 16-bit
+    format.fmt.pix.pixelformat = V4L2_PIX_FMT_YVU420;    // 8-bit
+    //format.fmt.pix.pixelformat = V4L2_PIX_FMT_Y16;       // 16-bit
     format.fmt.pix.width = 640;
     format.fmt.pix.height = 512;
 
